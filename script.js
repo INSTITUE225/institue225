30a992f6ae3978114656fc110c0021905b962f10
INSTITUE225 <maskpromusic@gmail.com>
2025-02-18 20:52:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function accesPrive() {
     window.location.href = "https://institue225.com"
 }
+// script.js
+
+/**
+ * Fonction déclenchée lors du clic sur le bouton "Accès Restreint".
+ * Redirige l'utilisateur vers la page de connexion.
+ */
+function accesPrive() {
+  window.location.href = 'login.html';
+}
